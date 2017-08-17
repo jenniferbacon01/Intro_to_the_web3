a5fcac140b2a5191a1c40e6b4510340596e79a2f
Abi Travers <atravers4007@gmail.com>
2017-08-17 15:45:42 +0100

    Adding scores to the play page

--- Battle/spec/features/testing_infrastructure_spec.rb
+++ Battle/spec/features/testing_infrastructure_spec.rb
@@ -6,6 +6,13 @@ require './app.rb'
 #     expect(page).to have_content 'Testing infrastructure working!'
 #   end
 # end
+#
+# before do
+#   visit('/')
+#   fill_in('name', with: 'catface')
+#   fill_in('name2', with: 'waterbottle')
+#   click_button('Submit')
+# end
 
 feature 'players entering names' do
   scenario 'player can enter name in form and see it on screen' do
@@ -14,5 +21,15 @@ feature 'players entering names' do
     fill_in('name2', with: 'waterbottle')
     click_button('Submit')
     expect(page).to have_content 'names are catface and waterbottle'
+  end
+end
+
+feature 'player1 seeing player2 hit points' do
+  scenario 'on play page hit points from players can be seen' do
+    visit('/')
+    fill_in('name', with: 'catface')
+    fill_in('name2', with: 'waterbottle')
+    click_button('Submit')
+    expect(page).to have_content "catface = 0, waterbottle = 0"
   end
 end
