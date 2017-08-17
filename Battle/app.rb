83e4be60253656b89301e08658687d65593389f8
Jenny Bacon <jenniferbacon01@gmail.com>
2017-08-17 13:45:14 +0100

    post redirect get pattern

--- Battle/app.rb
+++ Battle/app.rb
@@ -3,16 +3,27 @@ require 'shotgun'
 
 class MyApp < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/name' do
-    p params
-    @name = params[:name]
+    # p params
+    # @name = params[:name]
+    # @name2 = params[:name2]
+    session[:name] = params[:name]
+    session[:name2] = params[:name2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
+    @name2 = session[:name2]
     erb(:play)
   end
 
 
- run! if app_file == $0
+  run! if app_file == $0
 end
