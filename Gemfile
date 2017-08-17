caae7b4d9b2edfe391a9c73b52ffb2116b327b03
Abi Travers <atravers4007@gmail.com>
2017-08-17 12:00:25 +0100

    Battle repo

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
 ruby '2.4.0'
 
+gem 'shotgun'
 gem 'sinatra'
+gem 'capybara'
+gem 'selenium-webdriver'
--- app.rb
+++ app.rb
@@ -1,32 +1,29 @@
 require 'sinatra'
 require 'shotgun'
 
-def initialize
-  @rand_name
-end
-
 get '/' do
-  "Hello World"
+  "hello!"
 end
 
 get '/secret' do
-   "1= Hipity Hop Karaoke
-2 =Overexcited fat kid on a trampoline throwing up blue slushie and Harribo
-3= Noisy Kids on a bus
-5= Local Recyling Policy
-7= Outing to the Post Office
-9= Beetle Drive
-10= A Funeral"
+  "goodbye!"
+end
+
+get '/secretpath' do
+  "it was great to see you!"
 end
 
 get '/random-cat' do
-  @rand_name = params[:legends]
-  p params[:name]
-  erb :index
+  @name = ["Alessio","Giulio"].sample
+  erb(:index)
 end
 
-# post '/form' do
-#   @username = params[:username]
-#   puts params
-#   erb :form
-# end
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/naming-form' do
+  erb(:index)
+end
